3bd1044f5cddcb7ed07f95197dc0ff06d1a65b0c
Nocturnnn <nocturn2352@gmail.com>
2024-10-14 11:06:06 -0300

    Primeiros exercícios

---
+++ ex003.py
@@ -0,0 +1,4 @@
+num1 = int(input('digite um número: '))
+num2 = int(input('digite outro número: '))
+s = num1 + num2
+print('a some entre o primeiro número {} e o segundo número {} é igual a {}' .format(num1, num2, s))
